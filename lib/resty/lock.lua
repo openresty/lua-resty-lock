ece99fc0a3efcddb48777240f28111f80d00e0a8
Yichun Zhang (agentzh) <agentzh@gmail.com>
2018-06-05 13:52:56 -0700

    bumped version to 0.08.

--- lib/resty/lock.lua
+++ lib/resty/lock.lua
@@ -14,7 +14,7 @@ local debug = ngx.config.debug
 local setmetatable = setmetatable
 local tonumber = tonumber
 
-local _M = { _VERSION = '0.07' }
+local _M = { _VERSION = '0.08' }
 local mt = { __index = _M }
 
 local ERR = ngx.ERR
